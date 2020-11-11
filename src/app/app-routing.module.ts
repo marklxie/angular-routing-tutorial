310c56d9768ceb8b86820a1057120e8c9ffb47c9
Mark Xie <ly.markxie@gmail.com>
2020-11-11 15:04:22 -0500

    added routing and home pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { E404Component } from './e404/e404.component';
+import { EducationComponent } from './education/education.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full"},
+  { path: "home", component: HomeComponent},
+  { path:"about", component: AboutComponent},
+  { path:"help", component: HelpComponent},
+  { path:"work", component: WorkComponent},
+  { path:"education", component: EducationComponent},
+  { path: "**", component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpComponent } from './help/help.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { E404Component } from './e404/e404.component';
+import { WorkComponent } from './work/work.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpComponent,
+    HomeComponent,
+    AboutComponent,
+    E404Component,
+    WorkComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
